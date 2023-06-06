9fca8715f87b9f8bbf576f9d1b11c2894f6c91cd
Gabriel Bernardo Meireles <gbmreistom@gmail.com>
2023-06-06 20:41:09 -0300

    Part 2

--- main.go
+++ main.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("Parte 1")
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 {
+			fmt.Println(i)
+		}
+	}
+	fmt.Println("Parte 2")
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("PinPan")
+		} else if i%3 == 0 {
+			fmt.Println("Pin")
+		} else if i%5 == 0 {
+			fmt.Println("Pan")
+		} else {
 			fmt.Println(i)
 		}
 	}
